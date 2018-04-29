a4cee62cbef72d41779c5e0e7e01777d63a53f26
PCWhisperer <andrea.borin00@gmail.com>
2018-04-29 23:37:16 +0200

    Linear interpolation between timeline values

--- src/presepio.cpp
+++ src/presepio.cpp
@@ -1,7 +1,7 @@
 #include "presepio.h"
 
 // switch to simulate 50Hz signal
-const bool SIMULATE_VAC = false;
+const bool SIMULATE_VAC = true;
 
 // zero-crossings per second (signal ~ 50 Hz)
 const int RISE_PER_SECOND = 100;
@@ -14,20 +14,21 @@ const int TICKS_PER_RISE = TICKS_PER_SECOND / RISE_PER_SECOND;
 const int GATE_TICKS = TICKS_PER_RISE * 1 / 100;
 
 // 1/10sec
-const int TIMELINE_DURATION = 46;
+const int TIMELINE_DURATION = 40;
 
 const int ANALOGOUT_COUNT = 8;
 const int TIMELINE_ENTRIES = 10;
 const int TIME_PERCENT_PAIRS = TIMELINE_ENTRIES * 2;
 
-int analog_timeline[ANALOGOUT_COUNT][TIME_PERCENT_PAIRS] = {{0, 10, 5, 20, 10, 30, 15, 40, 20, 50, 25, 60, 30, 70, 35, 80, 40, 90, 45, 0},
-                                                            {0, 10, 5, 20, 10, 30, 15, 40, 20, 50, 25, 60, 30, 70, 35, 80, 40, 90, 45, 0},
-                                                            {0, 10, 5, 20, 10, 30, 15, 40, 20, 50, 25, 60, 30, 70, 35, 80, 40, 90, 45, 0},
-                                                            {0, 10, 5, 20, 10, 30, 15, 40, 20, 50, 25, 60, 30, 70, 35, 80, 40, 90, 45, 0},
-                                                            {0, 10, 5, 20, 10, 30, 15, 40, 20, 50, 25, 60, 30, 70, 35, 80, 40, 90, 45, 0},
-                                                            {0, 10, 5, 20, 10, 30, 15, 40, 20, 50, 25, 60, 30, 70, 35, 80, 40, 90, 45, 0},
-                                                            {0, 10, 5, 20, 10, 30, 15, 40, 20, 50, 25, 60, 30, 70, 35, 80, 40, 90, 45, 0},
-                                                            {0, 10, 5, 20, 10, 30, 15, 40, 20, 50, 25, 60, 30, 70, 35, 80, 40, 90, 45, 0}};
+// for t=0 and t=TIMELINE_DURATION percent values must be equal
+int analog_timeline[ANALOGOUT_COUNT][TIME_PERCENT_PAIRS] = {{0, 0, 5, 10, 10, 0, 15, 20, 20, 0, 25, 30, 30, 0, 35, 40, 40, 0, -1, 0},
+                                                            {0, 40, 5, 0, 10, 30, 15, 0, 20, 20, 25, 0, 30, 10, 35, 0, 40, 40, -1, 0},
+                                                            {0, 10, 5, 20, 10, 30, 15, 40, 20, 50, 25, 60, 30, 70, 35, 80, 40, 90, 50, 0},
+                                                            {0, 10, 5, 20, 10, 30, 15, 40, 20, 50, 25, 60, 30, 70, 35, 80, 40, 90, 50, 0},
+                                                            {0, 10, 5, 20, 10, 30, 15, 40, 20, 50, 25, 60, 30, 70, 35, 80, 40, 90, 50, 0},
+                                                            {0, 10, 5, 20, 10, 30, 15, 40, 20, 50, 25, 60, 30, 70, 35, 80, 40, 90, 50, 0},
+                                                            {0, 10, 5, 20, 10, 30, 15, 40, 20, 50, 25, 60, 30, 70, 35, 80, 40, 90, 50, 0},
+                                                            {0, 10, 5, 20, 10, 30, 15, 40, 20, 50, 25, 60, 30, 70, 35, 80, 40, 90, 50, 0}};
 
 Presepio::Presepio() : led_heartbeat(LED2),
                        analog0(D2),
@@ -86,6 +87,8 @@ void Presepio::dimming()
   //
   static int tick_per_rise_count = 0;
   static int dim_percent[ANALOGOUT_COUNT] = {0, 0, 0, 0, 0, 0, 0, 0};
+  // next timeline index entry to interpolate
+  static int dim_nextEntry_idx[ANALOGOUT_COUNT] = {1, 1, 1, 1, 1, 1, 1, 1};
 
   if (rise_received)
   {
@@ -111,34 +114,34 @@ void Presepio::dimming()
       curr_time += 1;
       //restart timeline
       if (curr_time == TIMELINE_DURATION)
-        curr_time = 0;
-    }
-
-    if (rise_received)
-    {
-      tick_per_rise_count = 0;
-      rise_received = false;
-      rise_count += 1;
-      if (rise_count == RISE_PER_SECOND)
       {
-        rise_count = 0;
-        led_heartbeat = !led_heartbeat;
+        curr_time = 0;
+        int out;
+        for (out = 0; out < ANALOGOUT_COUNT; out++)
+          dim_nextEntry_idx[out] = 1;
       }
     }
 
     //Update percent for each out
-    int out, time_value;
+    int out;
     for (out = 0; out < ANALOGOUT_COUNT; out++)
     {
-      for (time_value = 0; time_value < TIMELINE_ENTRIES; time_value++)
-      {
-        int t = analog_timeline[out][time_value * 2];
-        int v = analog_timeline[out][time_value * 2 + 1];
-        if (curr_time == t)
-        {
-          dim_percent[out] = v;
-        }
-      }
+      // interpolation between entries
+      int nextEntry_idx = dim_nextEntry_idx[out];
+      int prevEntry_idx = dim_nextEntry_idx[out] - 1;      
+
+      int prevT = analog_timeline[out][prevEntry_idx * 2];
+      int prevP = analog_timeline[out][prevEntry_idx * 2 + 1];
+
+      int nextT = analog_timeline[out][nextEntry_idx * 2];
+      int nextP = analog_timeline[out][nextEntry_idx * 2 + 1];
+
+      if (curr_time == nextT)
+        dim_nextEntry_idx[out] = dim_nextEntry_idx[out] + 1;
+      if (dim_nextEntry_idx[out] == TIMELINE_ENTRIES)
+        dim_nextEntry_idx[out] = 1;
+
+      dim_percent[out] = ((nextP - prevP) * (curr_time - prevT) / (nextT - prevT)) + prevP;
     }
 
     // set/reset each out based on percent
