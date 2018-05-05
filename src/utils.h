78c02248a053e5821e2afd5b77f1df5beb3bc814
xed89 <simone.bosc@gmail.com>
2018-05-05 14:43:21 +0200

    RelayBoard first implementation and cleanup of comments on main.cpp

---
+++ src/utils.h
@@ -0,0 +1,16 @@
+#ifndef _UTILS_H_
+#define _UTILS_H_
+
+class Utils {
+public:
+  static inline bool bitIsSet(uint8_t value, uint8_t bit) {
+    return (value & (1<<bit)) != 0;
+  }
+  static inline void nop() {
+    __ASM volatile (
+      "NOP\n\t"
+    );
+  }
+};
+
+#endif
